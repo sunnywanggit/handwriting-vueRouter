1c1f1280cb136bbba880682c6d185542834edd91
ergouwang <330035385@qq.com>
2020-08-11 09:36:15 +0800

    $router 注册到全局

--- src/router/MyRouter.js
+++ src/router/MyRouter.js
@@ -9,8 +9,6 @@ class HistoryRoute {
     constructor(props) {
         this.current = null;
     }
-
-
 }
 
 class MyRouter {
@@ -19,83 +17,112 @@ class MyRouter {
         this.mode = options.mode || hash;
         this.routes = options.routes || [];
         //传递过来的是一个路由表， 为了方便后期的处理，我们需要对这个数组做一个改造：{'/home':Home,'/main':Main}
-        this.routesMap = this.createMap(this.routes)
+        this.routesMap = this.createMap(this.routes);
         //路由中需要存放当前的路径 需要状态,之所以把它做成一个类，是为了方便我们后期进行扩展
         this.history = new HistoryRoute;
-        this.init()//开始初始化操作
+        this.init();//开始初始化操作
 
     }
-    init(){
+
+    init() {
         //首先需要判断路由使用的是什么模式
-        if(this.mode === 'hash'){
+        if (this.mode === 'hash') {
             //先判断用户打开时有没有 hash，如果再有就跳转到 #/
-            location.hash ? '' : location.hash = '/'
+            location.hash ? '' : location.hash = '/';
             //如果加载的时候就有hash值，我们就改变当前的url
-            window.addEventListener('load',()=>{
+            window.addEventListener('load', () => {
                 this.history.current = location.hash.slice(1);
-            })
+            });
             //监听hash的变化
-            window.addEventListener('hashchange',()=>{
-                this.history.current = location.hash.split(1)
-            })
-        }else{
-            location.pathname ? '' : location.pathname ='/';
-            window.addEventListener('load',()=>{
+            window.addEventListener('hashchange', () => {
+                this.history.current = location.hash.split(1);
+            });
+        } else {
+            location.pathname ? '' : location.pathname = '/';
+            window.addEventListener('load', () => {
                 this.history.current = location.hash.slice(1);
-            })
-            window.addEventListener('popstate',()=>{
+            });
+            window.addEventListener('popstate', () => {
                 this.history.current = location.hash.slice(1);
-            })
+            });
 
 
         }
 
     }
-    createMap(routes){
-        return routes.reduce((memo,current)=>{
+
+    createMap(routes) {
+        return routes.reduce((memo, current) => {
             memo[current.path] = current.component;
             return memo;
-        },{})//初始化 memo 为空对象
+        }, {});//初始化 memo 为空对象
     }
 
+    go() {
 
+    }
+
+    back() {
+
+    }
+
+    push() {
+
+    }
 }
-MyRouter.install = function (Vue,opts) {
+
+//当我们使用Vue.use(MyRouter)的时候，就会执行 MyRouter 的 install 方法
+MyRouter.install = function (Vue, opts) {
     //每个组件都有 this.$router 和 this.$route 属性
     //既然每个组件上都有这两个属性，那我们就使用混入来解决这个问题
-
     //在所有组件中获取同一个路由的实例
 
-
     Vue.mixin({
-        beforeCreate(){ //混合方法，会把这个方法和组件中的方法合成一个
-            Object.defineProperty(this,'$router',{ //$router 就是 Router 的实例 ,就是 Vue Router 的实例
-                get(){
-                    return {}
+        beforeCreate() { //mixin混合方法，会把这个方法和组件中的方法合成一个
+            if (this.$options && this.$options.router) { //定位跟组件
+                this._root = this; //把当前实例挂载到 _root
+                this._router= this.$options.router;//把 router 挂载到 _router 上
+            } else {
+                //如果不是根节点，那么就是子或者孙子节点,Vue 组件的渲染顺序是 父->子->孙
+                this._root = this.$parent._root;
+                //如果想获取唯一的路由实例 this._root._router
+
+            }
+            Object.defineProperty(this, '$router', {
+                //$router  ,就是 Vue Router 的实例
+                /*
+                问题来了？ 我们怎么才能在组件中拿到同一个路由的实例呢？
+                 */
+                //在所有组件中，获取同一个路由的实例
+                get() {
+                    //我们在这里就可以拿到惟一的路由实例
+                    return this._root._router;
                 }
-            })
-            Object.defineProperty(this,'$route',{
-                get(){
-                    return {}
+            });
+            Object.defineProperty(this, '$route', {
+                get() {
+                    return {};
                 }
-            })
+            });
         }
-    })
+    });
 
     //注册全局组件
-    Vue.component('router-link',{
+    Vue.component('router-link', {
         //h === createElement
-        render(h){
+        render(h) {
             //通过 h 渲染一个 a 标签，
-            return h('a',{},'首页')
+            return h('a', {}, '首页');
         }
-    })
-    Vue.component('router-view',{
-        render(h){
+    });
+    Vue.component('router-view', {
+        render(h) {
             //通过 h 渲染一个 a 标签，
-            return <h1>首页</h1>
+            return;
+        <
+            h1 > 首页 < /h1>;;
         }
-    })
-}
+    });
+};
 
 export default MyRouter;
